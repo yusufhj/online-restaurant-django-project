65e4254f299e648c8bd12998827d3f02c44818da
Yusuf AlHamad <ylive71@gmail.com>
2024-12-08 12:17:04 +0300

    edited the models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 from django.contrib.auth.models import User
 # Create your models here.
@@ -20,36 +21,33 @@ STATUS = (
     ('N', 'Not Paid')
 )
 
+class Order(models.Model):
+    status = models.CharField(max_length=1, choices=STATUS)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=7, decimal_places=3)
+    items = models.ManyToManyField('Menu', blank=True, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    categories = ArrayField(models.CharField(max_length=1, choices=CATEGORIES))
+    image = models.ImageField(upload_to='restaurant_images', blank=True, default=None)
+    orders_history = models.ManyToManyField(Order, blank=True, default=None)
+    
+    def __str__(self):
+        return self.name
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images', blank=True)
+    image = models.ImageField(upload_to='menu_images', blank=True, default=None)
     category = models.CharField(max_length=1, choices=CATEGORIES)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    categories = models.CharField(max_length=1, choices=CATEGORIES)
-    image = models.ImageField(upload_to='restaurant_images', blank=True)
-    menu = models.ManyToManyField(Menu)
-    orders_history = models.ManyToManyField('Order', blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Order(models.Model):
-    status = models.CharField(max_length=1, choices=STATUS)
-    date = models.DateField()
-    total = models.DecimalField(max_digits=5, decimal_places=2)
-    menu = models.ManyToManyField(Menu)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name

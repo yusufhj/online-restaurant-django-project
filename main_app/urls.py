3fc7825565531adf67c1e37a510594095cdd95d3
Yusuf AlHamad <ylive71@gmail.com>
2024-12-09 09:37:51 +0300

    added home(restaurant index), detail (restaurant detail page), and menu detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('detail/<int:restaurant_id>/', views.detail, name='detail'),
+    path('restaurant/<int:restaurant_id>/menu', views.detail, name='detail'),
+    path('restaurant/<int:restaurant_id>/menu/<int:menu_id>', views.menu_detail, name='menu_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,8 @@ def home(req):
 def detail(req, restaurant_id):
     restaurant = Restaurant.objects.get(id=restaurant_id)
     return render(req, 'detail.html', {'restaurant': restaurant, 'menus': Menu.objects.filter(restaurant=restaurant)})
+
+def menu_detail(req, restaurant_id, menu_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    menu = Menu.objects.get(id=menu_id)
+    return render(req, 'menu/menu_detail.html', {'restaurant': restaurant, 'menu': menu})
